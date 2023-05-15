238b63039190720bf156f0cde37c56d9cdb9e7b3
Temiloluwa1616 <sulaimantemiloluwa24@gmail.com>
2023-05-15 16:11:53 +0100

    react

---
+++ src/Treact.jsx
@@ -0,0 +1,16 @@
+import './treact.css'
+
+function Treact(){
+    return(
+<nav>
+        <h3>FRONT <em>ENDERZ</em></h3>
+        <ul>
+            <li><a href="">HOME</a></li>
+            <li><a href="">ABOUT</a></li>
+            <li><a href="">SKILLS</a></li>
+        </ul>
+    </nav>
+    )
+}
+
+export default Treact
